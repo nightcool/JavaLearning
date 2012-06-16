1b41766859c2c699e630753c507d8eeeef0fdb3e
itacademy <itacademy@192.168.1.8>
2012-06-16 21:44:20 +0900

    p9クラスの基本（続）

---
+++ src/jp/itacademy/samples/oop/ticket/Ticket.java
@@ -0,0 +1,28 @@
+package jp.itacademy.samples.oop.ticket;
+
+public class Ticket {
+
+	private int serial;
+	private String label;
+
+	public Ticket(int serial, String label) {
+		super();
+		this.serial = serial;
+		this.label = label;
+	}
+
+	public int getSerial() {
+		return serial;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+@Override
+public String toString() {
+	return "[Ticket] No." + serial + " " + label;
+	
+	
+	
+}}

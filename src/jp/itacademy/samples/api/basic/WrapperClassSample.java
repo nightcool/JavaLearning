c4c673ef7cc2397d3b9b001ff24551024599c632
itacademy <itacademy@192.168.1.10>
2012-06-30 21:39:59 +0900

    20120630

---
+++ src/jp/itacademy/samples/api/basic/WrapperClassSample.java
@@ -0,0 +1,23 @@
+package jp.itacademy.samples.api.basic;
+
+public class WrapperClassSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Integer i1 = new Integer(123);
+		System.out.println(i1);
+
+		int i2 = Integer.parseInt("123");
+		System.out.println(i2);
+
+		int i3 = new Integer(123);
+		System.out.println(i3);
+
+		Integer i4 = 123;
+		System.out.println(i4);
+	}
+
+}
